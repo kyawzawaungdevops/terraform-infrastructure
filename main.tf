874e23ee515a58b9bec1fa8022978a5838456843
kyawzawaung <80774788+kyawzawaungdevops@users.noreply.github.com>
2023-09-27 14:15:56 +0630

    Update main.tf

--- main.tf
+++ main.tf
@@ -201,8 +201,8 @@ resource "aws_instance" "Pub2b_ec2" {
 #Create an ALB target group
 resource "aws_lb_target_group" "alb-TG" {
   name     = "alb-TG"
-  port     = 443
-  protocol = "HTTPS"
+  port     = 80
+  protocol = "HTTP"
   vpc_id   = aws_vpc.my_vpc.id
 }
 
@@ -234,7 +234,7 @@ resource "aws_lb_listener" "lb_lst" {
 resource "aws_lb_target_group_attachment" "my-aws-alb" {
   target_group_arn = aws_lb_target_group.alb-TG.arn
   target_id        = aws_instance.Pub2a_ec2.id
-  port             = 443
+  port             = 80
 }
 
 #Load balancer-Target group attachment
